1266de0b1800ef5de352277e2ea4f9117c49e10c
Nelson Gustavo <nelsongborges@gmail.com>
2017-07-13 09:37:42 -0300

    Adding click events and search

--- src/index.js
+++ src/index.js
@@ -3,23 +3,38 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCuwg66J5hUVIDpry_vY-5VCKgsFvMU3UY';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({key: API_KEY, term: 'surboards'}, (videos) => {
-      this.setState({ videos });
+    this.videoSearch('surboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+         videos: videos,
+         selectedVideo: videos[0]
+       });
     });
   }
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
